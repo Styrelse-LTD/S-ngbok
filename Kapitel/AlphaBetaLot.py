debcb181094ffada0fec3f125986caaf33021a62
Styrelse-LTD <styrelse@ltd-mdu.com>
2023-12-27 19:14:45 +0100

    modified:   Kapitel/AlphaBetaLot.py 	modified:   Kapitel/Att Bonka med Shtil.tex 	modified:   Kapitel/Drinktips.tex 	modified:   "Kapitel/Egna p\303\245hitt.tex" 	modified:   Kapitel/Spel & lekar.tex 	modified:   main.aux 	deleted:    main.fdb_latexmk 	deleted:    main.fls 	modified:   main.log 	modified:   main.pdf 	modified:   main.synctex.gz 	modified:   main.toc 	modified:   setup.sty

--- Kapitel/AlphaBetaLot.py
+++ Kapitel/AlphaBetaLot.py
@@ -1,14 +1,16 @@
-f = open("main.lot", "r")
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-songs = []
-Name = ""
-for x in f.read():
-    if x == '%':
-        Name.removeprefix('\\contentsline {table}{')
-        songs.append(Name)
-        Name = ""
-    else:
-        Name = Name + x
-songs.sort
+'''
+Esse script organiza uma lista latex (toc) em ordem alfabética.
+Ncessário indicar o do arquivo com a lista na variável filename.
+'''
 
-print(songs)
+filename = 'main.toc'
+
+with open(filename) as f:
+    content = f.readlines()
+
+with open(filename, 'w') as f:
+    for i in sorted(content):
+        f.write(i)
